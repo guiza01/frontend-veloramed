f10e0630da915d50509fb5abffd6b441fb50ffa0
Jeferson Guilherme <jefersonguilherme.pe@gmail.com>
2025-03-13 15:47:32 -0300

    Adicionando tela de diaria, semanal e mensal da agenda do medico

--- src/app/(modules)/medassist/(content)/agenda/page.tsx
+++ src/app/(modules)/medassist/(content)/agenda/page.tsx
@@ -4,14 +4,15 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { eachDayOfInterval, startOfWeek, startOfMonth, endOfWeek, endOfMonth } from "date-fns";
-import { Filter, Search } from "lucide-react";
+import { ArrowRight, Filter, Search } from "lucide-react";
 import { useState } from "react";
 import { CgCloseR } from "react-icons/cg";
 import { FaArrowTrendDown, FaArrowTrendUp, FaRegSquareCheck } from "react-icons/fa6";
 import { MdMoneyOff } from "react-icons/md";
 import { format } from "date-fns";
 import { Dialog, DialogContent, DialogOverlay, DialogTitle } from "@/components/ui/dialog";
-
+import React from "react";
+import { Calendar } from "@/components/ui/calendar";
 
 const months = [
     "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
@@ -19,9 +20,10 @@ const months = [
 ];
 
 export default function Dashboard() {
-    const [selectedCategory, setSelectedCategory] = useState<'diario' | 'semanal' | 'mensal'>('diario');
+    const [selectedCategory, setSelectedCategory] = useState<'diária' | 'semanal' | 'mensal'>('mensal');
     const [search, setSearch] = useState('');
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [date, setDate] = React.useState<Date | undefined>(new Date());
 
     const [selectedDate] = useState(new Date());
     const [events, setEvents] = useState<Record<string, string[]>>({});
@@ -113,13 +115,13 @@ export default function Dashboard() {
             <div className="flex items-center justify-between">
                 <div className="gap-1">
                     <Button
-                        className={`mr-2 ${selectedCategory === 'diario'
+                        className={`mr-2 ${selectedCategory === 'diária'
                             ? 'bg-[#DEEAFF] text-[#2955D9]'
                             : 'bg-white text-black border border-gray-300 hover:bg-[#DEEAFF] hover:text-[#2955D9]'
                             }`}
-                        onClick={() => setSelectedCategory('diario')}
+                        onClick={() => setSelectedCategory('diária')}
                     >
-                        Diario
+                        Diaria
                     </Button>
                     <Button
                         className={`mr-2 ${selectedCategory === 'semanal'
@@ -143,11 +145,11 @@ export default function Dashboard() {
                 <div className="flex gap-4">
                     <div className="flex gap-4">
                         <div className="flex items-center gap-2">
-                            <div className="w-4 h-4 bg-[#F4AD27]"></div>
+                            <div className="w-4 h-4 rounded bg-[#F4AD27]"></div>
                             <p>Atendimentos particulares</p>
                         </div>
                         <div className="flex items-center gap-2">
-                            <div className="w-4 h-4 bg-[#08ADE6]"></div>
+                            <div className="w-4 h-4 rounded bg-[#08ADE6]"></div>
                             <p>Atendimentos por convênio</p>
                         </div>
                     </div>
@@ -165,42 +167,218 @@ export default function Dashboard() {
             </div>
 
             <div className="w-full mx-auto p-4">
-                <div className="grid grid-cols-7 gap-2">
-                    {["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SAB"].map((day) => (
-                        <div key={day} className="text-center border font-bold p-2">{day}</div>
-                    ))}
-                    {days.map((day) => (
-                        <div
-                            key={day.toString()}
-                            className="border p-2 h-24 relative cursor-pointer hover:bg-gray-100"
-                            onClick={() => handleAddEvent(format(day, "yyyy-MM-dd"))}
-                        >
-                            <span className="text-sm font-bold">{format(day, "dd")}</span>
-                            <div className="absolute inset-x-0 bottom-1 text-xs">
-                                {events[format(day, "yyyy-MM-dd")]?.map((event, index) => (
-                                    <div key={index} className="bg-blue-200 p-1 rounded mt-1">
-                                        {event}
+                {selectedCategory === 'mensal' && (
+                    <div className="grid grid-cols-7">
+                        {["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SAB"].map((day) => (
+                            <div key={day} className="text-center border font-bold p-2">{day}</div>
+                        ))}
+                        {days.map((day) => (
+                            <div
+                                key={day.toString()}
+                                className="border p-2 h-24 relative cursor-pointer hover:bg-gray-100"
+                                onClick={() => handleAddEvent(format(day, "yyyy-MM-dd"))}
+                            >
+                                <span className="text-sm font-bold">{format(day, "dd")}</span>
+                                <div className="absolute inset-x-0 bottom-1 text-xs">
+                                    {events[format(day, "yyyy-MM-dd")]?.map((event, index) => (
+                                        <div key={index} className="bg-blue-200 p-2 rounded mt-1">
+                                            {event}
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+
+                {selectedCategory === 'semanal' && (
+                    <div className="grid grid-cols-3 gap-4">
+                        <div>
+                            <div className="col-span-1 flex">
+                                <Calendar
+                                    mode="single"
+                                    selected={date}
+                                    onSelect={setDate}
+                                    className="rounded-md border"
+                                />
+                            </div>
+                            <div className="mt-4 border rounded p-3">
+                                <h1 className="mb-3 border-b">Status</h1>
+                                {[
+                                    { label: "Agendado", color: "#2955D9" },
+                                    { label: "Confirmado", color: "#22B257" },
+                                    { label: "Chegou", color: "#27B9F2" },
+                                    { label: "Cancelado", color: "#F23E2E" },
+                                    { label: "Faltou", color: "#F4AD27" },
+                                    { label: "Finalizado", color: "#9A9CA2" },
+                                ].map((status, index) => (
+                                    <div key={index} className="flex items-center px-2 gap-2 mt-2">
+                                        <div className="w-4 h-4 rounded" style={{ backgroundColor: status.color }}></div>
+                                        <h1>{status.label}</h1>
                                     </div>
                                 ))}
                             </div>
+                            <div className="mt-4 border rounded p-3">
+                                <h1 className="mb-3 border-b">Tipo de consulta</h1>
+                                <div className="px-2 ">
+                                    <div className="flex gap-2 items-center">
+                                        <div className="w-6 h-8 rounded flex items-center justify-center border bg-[#EBEBEC]">1</div>
+                                        <h1>Primeira consulta</h1>
+                                    </div>
+                                    <div className="flex gap-2 mt-4 items-center">
+                                        <div className="w-6 h-8 rounded flex items-center justify-center border bg-[#EBEBEC]">R</div>
+                                        <h1>Reagendamento</h1>
+                                    </div>
+                                    <div className="flex gap-2 mt-4 items-center hover:bg-[#FAFAFA]">
+                                        <div className="w-6 h-8 rounded flex items-center justify-center border bg-[#EBEBEC]">S</div>
+                                        <h1>Subsequente</h1>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    ))}
-                </div>
+                        <div className="col-span-2 grid grid-cols-7">
+                            {["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SAB"].map((day) => (
+                                <div key={day} className="text-center border font-bold p-2">{day}</div>
+                            ))}
+                            {days.map((day) => (
+                                <div
+                                    key={day.toString()}
+                                    className="border p-2 h-24 relative cursor-pointer hover:bg-gray-100"
+                                    onClick={() => handleAddEvent(format(day, "yyyy-MM-dd"))}
+                                >
+                                    <span className="text-sm font-bold">{format(day, "dd")}</span>
+                                    <div className="absolute inset-x-0 bottom-1 text-xs">
+                                        {events[format(day, "yyyy-MM-dd")]?.map((event, index) => (
+                                            <div key={index} className="bg-blue-200 p-1 rounded mt-1">
+                                                {event}
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                )}
+
+                {selectedCategory === 'diária' && (
+                    <div className="grid grid-cols-3 gap-4">
+                        <div className="col-span-1">
+                            <div className="">
+                                <Calendar
+                                    mode="single"
+                                    selected={date}
+                                    onSelect={setDate}
+                                    className="rounded-md border"
+                                />
+                            </div>
+                            <div className="mt-4 border rounded p-3">
+                                <h1 className="mb-3 border-b">Status</h1>
+                                {[
+                                    { label: "Agendado", color: "#2955D9" },
+                                    { label: "Confirmado", color: "#22B257" },
+                                    { label: "Chegou", color: "#27B9F2" },
+                                    { label: "Cancelado", color: "#F23E2E" },
+                                    { label: "Faltou", color: "#F4AD27" },
+                                    { label: "Finalizado", color: "#9A9CA2" },
+                                ].map((status, index) => (
+                                    <div key={index} className="flex items-center px-2 gap-2 mt-2">
+                                        <div className="w-4 h-4 rounded" style={{ backgroundColor: status.color }}></div>
+                                        <h1>{status.label}</h1>
+                                    </div>
+                                ))}
+                            </div>
+                            <div className="mt-4 border rounded p-3">
+                                <h1 className="mb-3 border-b">Tipo de consulta</h1>
+                                <div className="px-2 ">
+                                    <div className="flex gap-2 items-center">
+                                        <div className="w-6 h-8 rounded flex items-center justify-center border bg-[#EBEBEC]">1</div>
+                                        <h1>Primeira consulta</h1>
+                                    </div>
+                                    <div className="flex gap-2 mt-4 items-center">
+                                        <div className="w-6 h-8 rounded flex items-center justify-center border bg-[#EBEBEC]">R</div>
+                                        <h1>Reagendamento</h1>
+                                    </div>
+                                    <div className="flex gap-2 mt-4 items-center hover:bg-[#FAFAFA]">
+                                        <div className="w-6 h-8 rounded flex items-center justify-center border bg-[#EBEBEC]">S</div>
+                                        <h1>Subsequente</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-span-2 bg-[#FFFFFF]">
+                            <div className="border rounded p-2  w-full">
+                                <div className="flex items-end justify-between bg-[#FAFAFA] border-l-[4px] border-[#E7A113] rounded">
+                                    <div className="px-2">
+                                        <h1 className="mb-4">Rosana Guimarães dos Santos</h1>
+                                        <h1>Convênio - Unimed</h1>
+                                    </div>
+                                    <Button variant={'ghost'} className="flex items-center">
+                                        Ver prontuário
+                                        <ArrowRight />
+                                    </Button>
+                                </div>
+                                <div className="flex items-end mt-2 justify-between bg-[#FAFAFA] border-l-[4px] border-[#22B257] rounded">
+                                    <div className="px-2">
+                                        <h1 className="mb-4">Rosana Guimarães dos Santos</h1>
+                                        <h1>Convênio - Unimed</h1>
+                                    </div>
+                                    <Button variant={'ghost'} className="flex items-center">
+                                        Ver prontuário
+                                        <ArrowRight />
+                                    </Button>
+                                </div>
+                                <div className="flex items-end mt-2 justify-between bg-[#FAFAFA] border-l-[4px] border-[#27B9F2] rounded">
+                                    <div className="px-2">
+                                        <h1 className="mb-4">Rosana Guimarães dos Santos</h1>
+                                        <h1>Convênio - Unimed</h1>
+                                    </div>
+                                    <Button variant={'ghost'} className="flex items-center">
+                                        Ver prontuário
+                                        <ArrowRight />
+                                    </Button>
+                                </div>
+                                <div className="flex items-end mt-2 justify-between bg-[#FAFAFA] border-l-[4px] border-[#F23E2E] rounded">
+                                    <div className="px-2">
+                                        <h1 className="mb-4">Rosana Guimarães dos Santos</h1>
+                                        <h1>Convênio - Unimed</h1>
+                                    </div>
+                                    <Button variant={'ghost'} className="flex items-center">
+                                        Ver prontuário
+                                        <ArrowRight />
+                                    </Button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
 
             <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
                 <DialogOverlay className="fixed inset-0 bg-black/50" />
-                    <DialogContent className="fixed bg-white p-6 rounded-lg shadow-lg top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-h-[90vh] overflow-y-auto">
-                        <DialogTitle className="text-lg font-semibold mb-4">Detalhes do Agendamento</DialogTitle>
-                        <div className="grid grid-cols-2 gap-4">
-                            
-                        </div>
-                        <div className="flex items-center mt-4 justify-end">
-                            <Button onClick={() => setIsModalOpen(false)}>
-                                Fechar
-                            </Button>
-                        </div>
-                    </DialogContent>
+                <DialogContent className="fixed bg-white p-6 rounded-lg shadow-lg top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-h-[90vh] overflow-y-auto">
+                    <DialogTitle className="text-lg font-semibold mb-4">Detalhes do Agendamento</DialogTitle>
+                    <div className="grid grid-cols-2 gap-4">
+                        {/* Adicione informações do agendamento aqui */}
+                    </div>
+                    <div className="flex items-center mt-4 justify-end">
+                        <Button onClick={() => setIsModalOpen(false)}>Fechar</Button>
+                    </div>
+                </DialogContent>
+            </Dialog>
+
+            <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+                <DialogOverlay className="fixed inset-0 bg-black/50" />
+                <DialogContent className="fixed bg-white p-6 rounded-lg shadow-lg top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-h-[90vh] overflow-y-auto">
+                    <DialogTitle className="text-lg font-semibold mb-4">Detalhes do Agendamento</DialogTitle>
+                    <div className="grid grid-cols-2 gap-4">
+
+                    </div>
+                    <div className="flex items-center mt-4 justify-end">
+                        <Button onClick={() => setIsModalOpen(false)}>
+                            Fechar
+                        </Button>
+                    </div>
+                </DialogContent>
             </Dialog>
         </main>
     );
--- src/app/(modules)/medassist/(content)/historico/page.tsx
+++ src/app/(modules)/medassist/(content)/historico/page.tsx
@@ -1,11 +1,148 @@
 "use client";
 
-export default function Dashboard() {
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Dialog, DialogContent, DialogOverlay, DialogTitle } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { PaginationPrevious, Pagination, PaginationContent, PaginationEllipsis, PaginationNext } from "@/components/ui/pagination";
+import { ArrowLeft, ChevronLeft, ChevronRight, Filter, Search } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useState } from "react";
+
+const doctors = Array(5).fill({
+    index: 1,
+    name: 'Maria Elisângela dos Santos',
+    specialty: 'Endocrinologia',
+    convenio: 'Unimed',
+    tipeService: 'Consulta',
+    date: 'Ultima consulta: 12/12/2023',
+    dateTime: '12/12/2023 às 12:12',
+    plan: 'Convênio/Particular: Unimed',
+    value: 'R$350,00',
+});
+
+type PaginationItemProps = {
+    isActive?: boolean;
+    children: React.ReactNode;
+} & React.HTMLProps<HTMLLIElement>;
+
+const PaginationItem = ({ isActive, children, ...props }: PaginationItemProps) => (
+    <li {...props} className={`px-3 py-2 cursor-pointer ${isActive ? 'bg-blue-500 text-white' : 'text-gray-500'}`}>
+        {children}
+    </li>
+);
+
+export default function HistoricoPaciente() {
+    const [search, setSearch] = useState('');
+    const router = useRouter();
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     return (
-        <div>
-            <h1>Teste</h1>
-        </div>
+        <div className="mx-auto p-4 bg-[#FAFAFA]">
+            <div className="flex justify-between py-4">
+                <Button variant={'ghost'} onClick={() => router.push("/medassist/dashboard")}>
+                    <ArrowLeft size={18} />
+                    <h1 className="text-[20px]">Histórico de atendimentos</h1>
+                </Button>
+                <div className="flex items-center gap-4">
+                    <div className="relative w-full">
+                        <Search className="absolute left-3 top-2.5 text-gray-400" size={18} />
+                        <Input
+                            type="text"
+                            placeholder="Buscar"
+                            className="pl-10"
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                        />
+                    </div>
+                    <Button variant="outline">
+                        <Filter size={18} /> Filtrar
+                    </Button>
+                </div>
+            </div>
 
+            <div className="bg-[#FAFAFA] rounded-lg">
+                {doctors.map((doctor, index) => (
+                    <Card key={index} className="border-b mb-4">
+                        <CardContent className="flex items-center p-4 gap-4">
+                            <div className="flex-1">
+                                <p className="font-semibold">{doctor.name}</p>
+                                <p className="text-sm mt-2 text-gray-500">{doctor.date} | {doctor.plan} | {doctor.value}</p>
+                            </div>
+                            <Button variant="outline" onClick={() => setIsModalOpen(true)}>Detalhes</Button>
+                        </CardContent>
+                    </Card>
+                ))}
+            </div>
+
+            <div className="bg-[#FFFFFF] shadow flex justify-between items-center p-4 mt-4">
+                <PaginationPrevious className="text-gray-700 hover:bg-gray-200 p-2 rounded">
+                    <ChevronLeft size={18} className="mr-2" />
+                    Anterior
+                </PaginationPrevious>
+
+                <Pagination className="flex space-x-2">
+                    <PaginationContent>
+                        {[1, 2, '...', 9, 10].map((num, idx) => (
+                            <React.Fragment key={idx}>
+                                {num === '...' ? (
+                                    <PaginationEllipsis />
+                                ) : (
+                                    <PaginationItem
+                                        isActive={num === 1}
+                                        className={`px-3 py-1 rounded-full text-center cursor-pointer ${num === 1 ? 'bg-blue-500 text-white' : 'text-gray-700 hover:bg-gray-100'
+                                            }`}
+                                    >
+                                        {num}
+                                    </PaginationItem>
+                                )}
+                            </React.Fragment>
+                        ))}
+                    </PaginationContent>
+                </Pagination>
+
+                <PaginationNext className="text-gray-700 hover:bg-gray-200 p-2 rounded">
+                    Próximo
+                    <ChevronRight size={18} className="ml-2" />
+                </PaginationNext>
+            </div>
+
+            <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+                <DialogOverlay className="fixed inset-0 bg-black/50" />
+                {doctors.map((doctor, index) => (
+                    <DialogContent key={index} className="fixed bg-white p-6 rounded-lg shadow-lg top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-h-[90vh] overflow-y-auto">
+                        <DialogTitle className="text-lg font-semibold mb-4">Detalhes do Agendamento</DialogTitle>
+                        <div className="grid grid-cols-2 gap-4">
+                            <div>
+                                <h1 className="text-[#4D5056] text-[16px]">Nome</h1>
+                                <h1 className="mt-2 text-[20px]">{doctor.name}</h1>
+                            </div><div>
+                                <h1 className="text-[#4D5056]">Especialidade</h1>
+                                <h1 className="mt-2 text-[20px]">{doctor.specialty}</h1>
+                            </div><div>
+                                <h1 className="text-[#4D5056]">Tipo de Atendimento</h1>
+                                <h1 className="mt-2 text-[20px]">{doctor.convenio}</h1>
+                            </div><div>
+                                <h1 className="text-[#4D5056]">Tipo de Serviço</h1>
+                                <h1 className="mt-2 text-[20px]">{doctor.tipeService}</h1>
+                            </div><div>
+                                <h1 className="text-[#4D5056]">Valor</h1>
+                                <h1 className="mt-2 text-[20px]">{doctor.value}</h1>
+                            </div><div>
+                                <h1 className="text-[#4D5056]">Data e Hora</h1>
+                                <h1 className="mt-2 text-[20px]">{doctor.dateTime}</h1>
+                            </div>
+                        </div>
+                        <div className="flex items-center mt-4 justify-end">
+                            <Button onClick={() => setIsModalOpen(false)}>
+                                Fechar
+                            </Button>
+                        </div>
+                    </DialogContent>
+                ))}
+            </Dialog>
+
+        </div>
     );
 }
